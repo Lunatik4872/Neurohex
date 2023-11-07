5a6cb0c9add0abd073bc7bdb597b0f86a898b259
Lunatik4872 <gael.pailly@gmail.com>
2023-11-07 12:05:50 +0100

    maj 2

--- RNN.py
+++ RNN.py
@@ -15,12 +15,12 @@ class OneHot(tf.keras.layers.Layer):
 
 tf_inputs = tf.keras.Input(shape=(None,), batch_size=1) #Couche d'entré flexible qui prend n'importe quel taille de donné (J'ai pas fini la flexibilité)
 
-#one_hot = OneHot(4)(tf_inputs) #Je sais pas encore c'est quoi le meilleur traitement entre le one hot et le embedding
-embedding = tf.keras.layers.Embedding(input_dim=4, output_dim=64)(tf_inputs) #Créer un vecteur de taille identique pour tous (Le 4 represente les 4 valeurs de sortie possible) 
+one_hot = OneHot(4)(tf_inputs) #Je sais pas encore c'est quoi le meilleur traitement entre le one hot et le embedding
+#embedding = tf.keras.layers.Embedding(input_dim=4, output_dim=64)(tf_inputs) #Créer un vecteur de taille identique pour tous (Le 4 represente les 4 valeurs de sortie possible) 
 
-rnn_layer1 = tf.keras.layers.LSTM(128, return_sequences=True, stateful=True)(embedding) #Les diférentes couche du réseau RNN elles sont capable de mémoriser les états afin de jouer en fonction de ce qui c'est passé
-rnn_layer2 = tf.keras.layers.LSTM(128, return_sequences=True, stateful=True)(rnn_layer1) #Chaque nouvelle couche est relié à la précedente 
-hidden_layer = tf.keras.layers.Dense(128, activation="relu")(rnn_layer2) #Couche classique avec comme comme focntion la relu(x) = max(0,x)
+rnn_layer1 = tf.keras.layers.LSTM(20, return_sequences=True, stateful=True)(one_hot) #Couche du réseau RNN capable de mémoriser les états afin de jouer en fonction de ce qui c'est passé
+rnn_layer2 = tf.keras.layers.LSTM(20, return_sequences=True, stateful=True)(rnn_layer1) #Chaque nouvelle couche est relié à la précedente 
+hidden_layer = tf.keras.layers.Dense(20, activation="relu")(rnn_layer2) #Couche classique avec comme comme focntion la relu(x) = max(0,x)
 outputs = tf.keras.layers.Dense(4, activation="softmax")(hidden_layer) #sortie en proba softmax c'est comme la sigmoide mais en plus performant (Le 4 represente les 4 valeurs de sortie possible) 
 
 model = tf.keras.Model(inputs=tf_inputs, outputs=outputs)
@@ -63,7 +63,7 @@ def train() :
     for i in range(len(X)) :
         inputs,targets = X[i],Y[i]
 
-        for epoch in range(2000):
+        for epoch in range(1000): #le nombre de neurones doit etre 10 et 90 j'ai pas assez testé pour une val exact
             for batch_inputs, batch_targets in zip(inputs, targets):
                 #On converti les données en matrices tensor et on les aplati
                 train_step(tf.reshape(tf.convert_to_tensor(batch_inputs),(1,-1)), tf.reshape(tf.convert_to_tensor(batch_targets),(1,-1))) 
@@ -74,7 +74,7 @@ def train() :
 
     model.save('my_model.keras')
 
-#train() #Pense à enlever le com pour entrainer le réseau 
+train() #Pense à enlever le com pour entrainer le réseau 
 
 def IA_jouer(tab) :
     #Un traitement suplémentaire pour les sortir de la forme tensor qui est pas jolie à voir et assez discrete 
@@ -83,7 +83,9 @@ def IA_jouer(tab) :
     return np.reshape(jeu.numpy(),(dim,dim))
 
 model.load_weights('my_model.keras') #C'est le fichier ou on stock le réseau RNN 
-jeu = [[2,1,1,2],[2,1,1,2],[2,1,1,2],[2,1,1,2]]
+jeu = [[3,0,2],
+       [0,3,3],
+       [2,0,2]] #Aie de l'overfitting bon faut jouer avec le nombre de noeurones et le nombre d'iteration pour l'apprentissage (S'il recopie betement les données c'est qu'il est en surapprentissage en gros il ne décide plus par lui même il ce base uniquement sur les données)
 
 next_move = IA_jouer(jeu)
 
